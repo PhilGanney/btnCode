c398b249e8e9b84c27e906880a8e7c2aa5877f2b
Phil Ganney <philipganney@gmail.com>
2022-10-03 12:48:03 +0100

    Milestone: Functional Demo!
    Gonna take a good break now!

--- btnCodeUi.js
+++ btnCodeUi.js
@@ -71,10 +71,43 @@ function makeCode(key){
 	//get the text
 	console.log(savedCode[btnClicked.className][key]);
 	
-	
+	//insertTextAtCursor(el, text)
+	insertTextAtCursor(codeTA, savedCode[btnClicked.className][key]);
 	
 	console.groupEnd();
 }
+
+
+/*I had no idea how to find the cursor so searched online and got this entire function that was doing what I wanted to do ultimately:
+https://stackoverflow.com/questions/7404366/how-do-i-insert-some-text-where-the-cursor-is
+
+Notes:
+Seems that this solution has to account for browser differences - this is what the top level if test is about
+
+Stuff I did not recognise:
+ownerDocument: https://www.w3schools.com/jsref/prop_node_ownerdocument.asp
+selectionStart
+
+*/
+function insertTextAtCursor(el, text) {
+    var val = el.value, endIndex, range, doc = el.ownerDocument;
+    if (typeof el.selectionStart == "number"
+            && typeof el.selectionEnd == "number") {
+		//Not IE
+        endIndex = el.selectionEnd;
+        el.value = val.slice(0, endIndex) + text + val.slice(endIndex);
+        el.selectionStart = el.selectionEnd = endIndex + text.length;
+    } else if (doc.selection != "undefined" && doc.selection.createRange) {
+		//IE
+        el.focus();
+        range = doc.selection.createRange();
+        range.collapse(false);
+        range.text = text;
+        range.select();
+    }
+}
+
+
 function someFunction(){
 
 }
