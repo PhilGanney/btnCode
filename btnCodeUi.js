9e780696c7102a9bf67e75dfa072dee8b1deca1a
Phil Ganney <philipganney@gmail.com>
2022-10-03 09:23:55 +0100

    js btns trigger makeCode(key)

--- btnCodeUi.js
+++ btnCodeUi.js
@@ -1,9 +1,37 @@
+var savedCode = {
+	"JS": {
+		"function": "function changeMe1(){}",
+		"variable": "var changeMe1;",
+		"constant": "const changeMe1;",
+		"ifElse": "if(){} else {}"
+	}
+	
+}
+
 function pageLoad(){
 	console.groupCollapsed("page load");
+	btnJSFunc.addEventListener("click", function(){ makeCode('function')});
+	btnJSVar.addEventListener("click", function(){ makeCode('variable')});
+	btnJSConst.addEventListener("click", function(){ makeCode('constant')});
+	btnJSIfElse.addEventListener("click", function(){ makeCode('ifElse')});
+	
+	
+	
 	console.groupEnd();
 }
 
 
+function makeCode(key){
+	console.groupCollapsed("makeCode: " + key);
+	
+	
+	
+	
+	console.groupEnd();
+}
+function someFunction(){
+
+}
 function someFunction(){
 
 }
